bc91ff5e7a6e9914dca951c6663e67b06949d823
codewithsana <122785459+codewithsana@users.noreply.github.com>
2023-02-03 14:15:24 +0530

    Add files via upload

---
+++ 2nd feb lab/Arithmetic.java
@@ -0,0 +1,19 @@
+package LAB;
+public class Arithmetic {//create class
+	public static void main(String[] args) {//main method
+		int a=125,b=24,sum,sub,mul,div,mod;
+		System.out.println("the first value is:"+a);
+		System.out.println("the second value is:"+b);
+		sum=a+b;//addition
+		System.out.println("the sum is:"+sum);
+		sub=a-b;//subtraction
+		System.out.println("the sub is:"+sub);
+		mul=a*b;//multiplication
+		System.out.println("the mul is:"+mul);
+		div=a/b;//division
+		System.out.println("the div is:"+div);
+		mod=a%b;//modulus
+		System.out.println("the mod is:"+mod);
+	}
+
+}
---
+++ 2nd feb lab/ExpressionProgram.java
@@ -0,0 +1,10 @@
+package LAB;
+
+public class ExpressionProgram {
+public static void main(String[] args) {
+	double a=25.5,b=3.5,c=40.5,d=4.5,exp;
+	exp =(a*b-b*b)/(c-d);
+	//exp=(25.5*3.5-3.5*3.5)/(40.5-4.5);
+	System.out.println("the expression is:"+exp);
+}
+}
