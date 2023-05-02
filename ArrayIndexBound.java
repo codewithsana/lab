38bbcfe96d7c17ed5058b928cc0d49f1dc629833
codewithsana <122785459+codewithsana@users.noreply.github.com>
2023-05-02 18:01:52 +0530

    Delete ArrayIndexBound.java

--- ArrayIndexBound.java
+++
@@ -1,9 +0,0 @@
-package Array;
-
-public class ArrayIndexBound {
-public static void main(String[] args) {
-	int a[]= {2,5,4,8,9};
-	for(int i=0;i<=a.length;i++) {
-	System.out.println(a[i]);	
-	}
-}}
